2aaf9dd2a84e584370e6fd074d1be1b9ff4028f8
ykovalenkoiconsoft <ykovalenko@iconsoft.ru>
2022-07-23 21:26:38 +0300

    two apps from lesson #1

---
+++ lesson_1/MyFirstApp.java
@@ -0,0 +1,5 @@
+public class MyFirstApp {
+	public static void main(String[] args) {
+		System.out.println("Yury, please, start learning Java once again!, and then use it anywhere!!!");
+	}
+}
---
+++ lesson_1/VariablesTheme.java
@@ -0,0 +1,26 @@
+public class VariablesTheme {
+	
+	public static void main(String[] args) { 
+
+		// TODO 1. Создание переменных и вывод их значений на консоль (это название задачи)
+		System.out.println("1. Создание переменных и вывод их значений на консоль (это название задачи)");
+		System.out.println();
+
+		byte cores = 4;
+		short memory = 8;
+		int clock = 2400;
+
+		System.out.println("cores = " + cores); 
+		System.out.println("memory = " + memory);  
+		System.out.println("Clock = " + clock + "  MHz");
+
+
+		/*
+
+          TODO 2. Расчет стоимости 
+                  товара со скидкой
+
+		*/
+
+	} 
+}
